670620a9eb97057e17ef78a1bda39c672e8a1074
dennykurniawan48 <kurniawan.denny.48.com>
2021-10-20 23:05:46 +0700

    Fixing Style and added coil for image library

--- app/src/main/java/com/signaltekno/paging3androidcompose/MainActivity.kt
+++ app/src/main/java/com/signaltekno/paging3androidcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -17,8 +18,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.signaltekno.paging3androidcompose.ui.theme.Paging3AndroidComposeTheme
 import com.signaltekno.paging3androidcompose.viewmodel.PostViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -39,10 +43,20 @@ class MainActivity : ComponentActivity() {
             }else if(result is NetworkResult.Error){
                 Text("Error showing data", modifier = Modifier.size(40.dp))
             }else if(result is NetworkResult.Success) {
-                LazyColumn {
+                LazyColumn(Modifier.padding(4.dp)) {
                     if (result != null) {
                         items(result!!.data!!.results) {
-                            Text(it.name)
+                            Row(modifier = Modifier.padding(top=4.dp)){
+                                Image(
+                                    painter = rememberImagePainter(it.image),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(80.dp)
+                                )
+                                Column(modifier = Modifier.height(80.dp).padding(start=16.dp), verticalArrangement = Arrangement.Center) {
+                                    Text(text = it.name, style = MaterialTheme.typography.body1, fontWeight = FontWeight.Bold)
+                                    Text(text = it.species + " " + it.gender, style = MaterialTheme.typography.subtitle2)
+                                }
+                            }
                         }
                     }
                 }
