cdaec0102afc7749fbb035b14d7df1a53037fc3c
dennykurniawan48 <kurniawan.denny.48.com>
2021-10-21 00:09:39 +0700

    Fixing null pointer exception when enter the last page

--- app/src/main/java/com/signaltekno/paging3androidcompose/CharacterPagingSource.kt
+++ app/src/main/java/com/signaltekno/paging3androidcompose/CharacterPagingSource.kt
@@ -1,5 +1,6 @@
 package com.signaltekno.paging3androidcompose
 
+import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.signaltekno.paging3androidcompose.model.Characters
@@ -15,10 +16,10 @@ class CharacterPagingSource @Inject constructor(private val repository: Reposito
             val page = params.key ?: 1
 
             val response = repository.getCharacter(page)
-            val characters: List<Result> = response!!.data!!.results
+            var characters: List<Result> = if(response is NetworkResult.Success) response.data!!.results else emptyList()
 
             val prevKey = if (page > 0) page - 1 else null
-            val nextKey = if (response.data!!.info.next != null) page + 1 else null
+            val nextKey = if (response is NetworkResult.Success && response.data!!.info.next != null) page + 1 else null
 
             LoadResult.Page(data = characters, prevKey = prevKey, nextKey = nextKey)
         } catch (exception: IOException) {
